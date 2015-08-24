bbfba42c7fbf3f727c073ef480a6d302752f066e
Adrien Josso <josso.adrien@gmail.com>
2015-08-24 22:14:55 +0200

    archetype creation from interface project

---
+++ src/main/resources/archetype-resources/wui-generic-interface/test/IntegrationTest.java
@@ -0,0 +1,32 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+import org.junit.*;
+
+import play.Play;
+import play.mvc.*;
+import play.test.*;
+import play.libs.F.*;
+
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+public class IntegrationTest {
+
+    /**
+     * add your integration test here
+     * in this example we just check if the welcome page is being shown
+     */   
+    @Test
+    public void test() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+            public void invoke(TestBrowser browser) {
+                browser.goTo("http://localhost:3333");
+                assertThat(browser.pageSource()).contains(Play.application().configuration().getString("app.name"));
+            }
+        });
+    }
+  
+}
